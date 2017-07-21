cd9deaee9b2110c77c97b9465327658df6932deb
Coen Mooij <coenmooij@gmail.com>
2017-07-21 21:07:59 +0200

    fix logo size

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <router-link tag="a" class="navbar-left" :to="{name:'ServiceProfiles'}">
-                <img alt="Eagle" src="../assets/logo-white.png" height="48px">
+                <img alt="Eagle" src="../assets/logo-white.png" height="42px" style="padding:6px 0 0 0;">
             </router-link>
         </div><!-- /.container-fluid -->
     </nav>
