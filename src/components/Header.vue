e0b538ed12c4adc6459009c12a0d2ce5a57b57c9
Coen Mooij <coenmooij@gmail.com>
2017-07-24 20:18:58 +0200

    align header with content

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,13 @@
 <template>
     <nav class="navbar navbar-default">
         <div class="container">
+
+
+        <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
                 <router-link tag="a" class="navbar-left" :to="{name:'JobProfiles'}">
                     <img alt="Eagle" src="../assets/logo-blue.png">
                 </router-link>
+        </div>
         </div>
     </nav>
 </template>
@@ -16,6 +20,6 @@
     }
 
     a img {
-        margin: 20px 0;
+        margin: 20px -20px;
     }
 </style>
