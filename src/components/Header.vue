fcff890974b9a71b427f94ceaadb0d455b670e53
Coen Mooij <coenmooij@gmail.com>
2017-08-13 15:21:15 +0200

    add logout functionality and implement resetPassword

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,103 @@
 <template>
     <nav class="navbar navbar-default">
         <div class="container">
-
-
-        <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+            <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
                 <router-link tag="a" class="navbar-left" :to="{name:'JobProfiles'}">
                     <img alt="Trukker logo" src="../assets/trukker-logo.png">
                 </router-link>
-        </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown" ref="dropdown" :class="{open: isDropdownOpen}" @click="toggleDropdown">
+                        <a href="#" @click.prevent="" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                           aria-haspopup="true"
+                           aria-expanded="false">
+                            <img id="avatar" class="img-circle"
+                                 src="https://www.lavozdegalicia.es/img/avatar/user_male.png" width="40px"
+                                 height="40px"><span
+                                class="caret"> </span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" @click.prevent="logout"><strong
+                                    class="glyphicon glyphicon-log-out"></strong> Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </template>
 
+<script>
+    import {mapActions} from 'vuex';
+
+    export default {
+        data(){
+            return {
+                isDropdownOpen: false,
+            };
+        },
+        methods: {
+            ...mapActions({
+                clearToken: 'clearToken'
+            }),
+            toggleDropdown(){
+                this.isDropdownOpen = !this.isDropdownOpen;
+            },
+            resetDropdown(e){
+                let el = this.$refs.dropdown;
+                let target = e.target;
+                if (( el !== target) && !el.contains(target)) {
+                    this.isDropdownOpen = false;
+                }
+            },
+            logout(){
+                this.$http.get('authentication/logout')
+                    .then(response => {
+                        response.json()
+                    }).then(data => {
+                    this.clearToken();
+                    this.$router.push('/');
+                }).catch(error => {
+                    this.clearToken();
+                    this.$router.push('/');
+                });
+            }
+        },
+        created() {
+            document.addEventListener('click', this.resetDropdown)
+        },
+        destroyed() {
+            document.removeEventListener('click', this.resetDropdown)
+        }
+    }
+</script>
+
 <style scoped>
+
+    .img-circle {
+        margin: 0;
+        padding: 0;
+    }
+
+    a.dropdown-toggle {
+        padding: 21px 11px;
+    }
+
+    .navbar-default .navbar-nav > .open > a,
+    .navbar-default .navbar-nav > .open > a:focus,
+    .navbar-default .navbar-nav > .open > a:hover {
+        background-color: #f2f2f3;
+    }
+
+    #avatar {
+        margin-right: 5px;
+        border: 1px solid #f2f2f3;
+    }
+
+    .caret {
+        margin-left: 5px;
+    }
+
     nav {
         background-color: #fff;
         margin-left: auto;
@@ -20,6 +105,6 @@
     }
 
     a img {
-        margin:20px -20px;
+        margin: 20px -20px;
     }
 </style>
--- src/components/authentication/PasswordReset.vue
+++ src/components/authentication/PasswordReset.vue
@@ -2,13 +2,22 @@
     <div class="password-reset">
         <div class="panel panel-default">
             <div class="panel-body">
+                <div class="alert alert-success text-center" v-if="showSuccessMessage">
+                    <p><span class="glyphicon glyphicon-ok"></span> Instructies om je wachtwoord te herstellen zijn verzonden naar het emailadres dat je hebt opgegeven.</p><br>
+                    <button class="btn btn-success" @click="redirect">Naar inloggen</button>
+                </div>
                 <form>
                     <div class="form-group">
-                        <label for="email">Emailadres</label>
-                        <input class="form-control" id="email" type="text">
+                        <label for="email">Emailadres*</label>
+                        <input class="form-control" id="email" name="Emailadres" type="text" v-model.lazy="email"
+                               v-validate="'required|email'">
+                        <small v-if="showErrors && errors.has('Emailadres')" class="form-text text-danger"><span
+                                class="glyphicon glyphicon-remove help is-danger"></span>
+                            {{ errors.first('Emailadres') }}
+                        </small>
                     </div>
                     <div class="divider"></div>
-                    <button type="submit" class="btn btn-danger pull-right" @click.prevent="reset">
+                    <button type="submit" class="btn btn-danger pull-right" @click.prevent="submit">
                         <span class="glyphicon glyphicon-repeat"></span> Reset Wachtwoord
                     </button>
                     <div class="clearfix"></div>
@@ -33,10 +42,36 @@
 
 <script>
     export default {
+        data(){
+            return {
+                email: '',
+                showSuccessMessage: false,
+                showErrors: false,
+            };
+        },
         methods: {
+            redirect(){
+                this.$router.push('/');
+            },
+            submit(){
+                this.$validator.validateAll().then(result => {
+                    if (result) {
+                        this.reset();
+                    } else {
+                        this.showErrors = true;
+                    }
+                });
+            },
             reset(){
-                console.log('reset password');
-                this.$router.push({name: 'Login'});
+                this.$http.post('authentication/resetPassword', {
+                    email: this.email,
+                }).then(response => {
+                    response.json()
+                }).then(data => {
+                    this.showSuccessMessage = true;
+                }).catch(error => {
+                    this.error = error;
+                });
             },
         },
     }
